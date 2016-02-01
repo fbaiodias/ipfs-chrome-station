7cf18e4206ca97dcbe0cb856d9dbc1d932653caf
Francisco Baio Dias <xicombd@gmail.com>
2016-02-01 01:41:10 +0000

    Better interceptor start and stop logic

--- chrome/app/background/interceptor.js
+++ chrome/app/background/interceptor.js
@@ -18,6 +18,7 @@ function interceptor (details) {
 }
 
 function startInterceptor () {
+  console.log('starting interceptor')
   chrome.webRequest.onBeforeRequest.addListener(interceptor,
     { urls: ['*://*/ipfs/*', '*://*/ipns/*'] },
     ['blocking']
@@ -25,23 +26,41 @@ function startInterceptor () {
 }
 
 function stopInterceptor () {
+  console.log('stopping interceptor')
   chrome.webRequest.onBeforeRequest.removeListener(interceptor)
 }
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
   Object.keys(changes).forEach(key => {
     var storageChange = changes[key]
-    if (storageChange.newValue === true) {
-      startInterceptor()
-    } else {
-      stopInterceptor()
+    if (key === 'redirecting') {
+      if (storageChange.newValue === true) {
+        startInterceptor()
+      } else {
+        stopInterceptor()
+      }
+    } else if (key === 'running') {
+      if (storageChange.newValue === true) {
+        chrome.storage.sync.get('redirecting', function (result) {
+          if (result.redirecting === true) {
+            startInterceptor()
+          }
+        })
+      } else {
+        stopInterceptor()
+      }
     }
   })
 })
 
-chrome.storage.sync.get('redirecting', function (result) {
-  console.log('get redirecting', result)
-  if (result.redirecting === true) {
-    startInterceptor()
+chrome.storage.sync.get('redirecting', ({ redirecting }) => {
+  console.log('get redirecting', redirecting)
+  if (redirecting === true) {
+    chrome.storage.local.get('running', ({ running }) => {
+      console.log('get running', running)
+      if (running === true) {
+        startInterceptor()
+      }
+    })
   }
 })
